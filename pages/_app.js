420445390dabd49360ad48de45246e7af01f1172
reiearth <reionlinebiz@gmail.com>
2022-01-24 05:51:40 +0800

    fixed Material UI Error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { useEffect }  from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    //Fixed SSR Material UI Styles
+   // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  },[]);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- utils/styles.js
+++ utils/styles.js
@@ -9,6 +9,13 @@ import { makeStyles } from "@material-ui/core";
 
         },
     },
+    brand: {
+        fontWeight:'bold',
+        fontSize: '1.5rem',
+    },
+    grow: {
+        flexGrow: 1
+    },
     main: {
         minHeight: '80vh',
     },
